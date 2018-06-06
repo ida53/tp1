b63e3bf64fd695b84b1af85b31170a509eb21d80
ida53 <i.dagano@iia-laval.fr>
2018-06-06 15:41:37 +0200

    first commit

---
+++ src/main/java/com/testFactory/tp1/test.java
@@ -0,0 +1,19 @@
+package com.testFactory.tp1;
+import org.joda.time.DateTime;
+
+public class test{
+	
+	public static void main(String[] args){
+		
+		
+		DateTime date = DateTime.now();
+		
+		System.out.println("coucou!!!! "+date);
+		
+	}
+
+	public static int add(int i, int j) {
+		// TODO Auto-generated method stub
+		return i+j;
+	}
+}
---
+++ src/test/java/com/testFactory/tp1/testTest.java
@@ -0,0 +1,21 @@
+package com.testFactory.tp1;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testTest {
+
+	@Test
+	void test() {
+		int i=2 , j=3;
+		
+		int k = test.add(i,j);
+		
+		assert.assertEquals("Erreur!! ", k, 5);
+		
+		
+	}
+
+}
