c7894d7e734ad6cdebe818540188478ea29d07e8
ida53 <i.dagano@iia-laval.fr>
2018-06-07 09:39:07 +0200

    Modif test

--- src/test/java/com/testFactory/tp1/testTest.java
+++ src/test/java/com/testFactory/tp1/testTest.java
@@ -3,17 +3,18 @@ package com.testFactory.tp1;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 class testTest {
 
 	@Test
 	void test() {
-		int i=2 , j=3;
+		int i=3 , j=3;
 		
 		int k = test.add(i,j);
 		
-		assert.assertEquals("Erreur!! ", k, 5);
+		Assert.assertEquals("Erreur!! ", k, 6); 
 		
 		
 	}
