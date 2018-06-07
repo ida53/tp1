adee44fbd8cc744546978adedde27d6f7d285957
ida53 <i.dagano@iia-laval.fr>
2018-06-07 16:34:53 +0200

    Modif Test

--- src/test/java/com/appfactory/tp1/ApplicationTest.java
+++ src/test/java/com/appfactory/tp1/ApplicationTest.java
@@ -1,17 +1,28 @@
 package com.appfactory.tp1;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.appfactory.tp1.Application;
 
 public class ApplicationTest {	
-	
+
+    @Before
+    public void setup() {
+        System.out.println("Pre-test");
+    }
+
+    @After
+    public void teardown() {
+
+    }
+
 	@Test
 	public void test() {
 		final int i=2, j=3;
-		final int k = Application.add(i, j);
-		
+		final int k = Application.add(i, j);		
 		Assert.assertEquals("Erreur ", k, 5);
 	}
 
